78d5705e67f11247a2ea710f7e78aed3aadfaecc
4blogga <4blogga@gmail.com>
2019-03-27 16:49:36 +1000

    1

--- js/index.js
+++ js/index.js
@@ -63,7 +63,7 @@ function onDeviceReady() {
 	// var ref = cordova.InAppBrowser.open('https://directdv.ru/upload-content', '_self', 'location=yes,zoom=no');
 	
 	// window.open('https://google.com', '_self ', 'location=yes');
-	// window.open('https://directdv.ru/upload-content', '_self ', 'location=yes');
+	window.open('https://directdv.ru/upload-content', '_self ', 'location=yes');
 	
 /* 	ref.addEventListener('loadstart', inAppBrowserbLoadStart);
 	ref.addEventListener('loadstop', inAppBrowserbLoadStop);
